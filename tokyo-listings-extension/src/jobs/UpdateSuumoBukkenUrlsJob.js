005fb0ff17efec73feb690b3059a028c4eaa0b90
Mitchell Smith <mitchellwsmith@hotmail.com>
2021-12-24 17:37:24 -0800

    Added UpdateSuumoBukkenUrlJob
    Suumo page for a property (bukken) display listings for that property. For each real estate company there is a separate listing page (chintai/bc_*). These 'bc' pages map many-to-few for overall listing pages (chintai/jnc_*). This job updates the href of these links on the bukken page to point to the 'jnc' listing pages. It also removes duplicates on the bukken page.

---
+++ tokyo-listings-extension/src/jobs/UpdateSuumoBukkenUrlsJob.js
@@ -0,0 +1,31 @@
+export default class UpdateSuumoBukkenUrlsJob {
+
+  constructor(scrapedElems) {
+    this.scrapedElems = scrapedElems
+    this.endpoint = "http://localhost:8082/api/listing/suumoBukken/"
+  }
+
+  buildPayload() {
+    return {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({urls: this.scrapedElems.map(l => l.url)})
+    }
+  }
+
+  execute() {
+    fetch(this.endpoint, this.buildPayload())
+      .then(res => res.json())
+      .then(out => {
+        this.scrapedElems.forEach(l => {
+          let linkElems = Array.from(l.listingElem.getElementsByTagName("a"))
+          linkElems.forEach(a => a.setAttribute('href', out[l.url]))
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+}
