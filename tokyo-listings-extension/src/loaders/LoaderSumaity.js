2533e19904a720952034a29cc813b4765d01600e
mwsmws22 <github@sl.mws.tokyo>
2021-12-28 01:16:43 -0800

    Added loader for sumaity

---
+++ tokyo-listings-extension/src/loaders/LoaderSumaity.js
@@ -0,0 +1,24 @@
+export default class LoaderSumaity {
+
+  constructor() {
+    this.pipeline = ['remove archived listings', 'highlight similar listings']
+    this.scrapedElems = []
+  }
+
+  execute() {
+    const listings = Array.from(document.getElementById('result').children)
+                          .filter(div => div.className === 'building')
+
+    listings.forEach(div =>
+      this.scrapedElems.push({
+        propertyElem: div,
+        address: div.querySelector('.address').innerText,
+        listings: Array.from(div.querySelectorAll('tr.estate.applicable')).map(tr => ({
+          listingElem: tr,
+          key: tr.querySelector('a.detailBtn').href.match(/prop_(.*?)\//)[1],
+          square_m: tr.querySelector('td.type').children[1].innerText.replace("m2", "")
+        }))
+      })
+    )
+  }
+}
