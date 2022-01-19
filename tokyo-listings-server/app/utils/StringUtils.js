eae6d965803cdc1c8a6b0696fe1282bb18799588
mwsmws22 <github@sl.mws.tokyo>
2022-01-19 07:40:16 -0800

    Update to TatoDesign scraper and StringUtils

--- tokyo-listings-server/app/utils/StringUtils.js
+++ tokyo-listings-server/app/utils/StringUtils.js
@@ -7,12 +7,28 @@ module.exports = class StringUtils {
   }
 
   static noSpaces() {
-    return this.replace(/\s/g, '')
+    return this.remove(' ')
+  }
+
+  static remove(...args) {
+    const reduceFunc = (prev, curr) => prev.replace(new RegExp(curr, 'g'), '')
+    return args.reduce(reduceFunc, this)
+  }
+
+  static float() {
+    return String(parseFloat(this))
+  }
+
+  static toManen() {
+    return parseInt(this) / 10000
   }
 
   static initialize() {
     String.prototype.convertHalfWidth = this.convertHalfWidth
     String.prototype.noSpaces = this.noSpaces
+    String.prototype.remove = this.remove
+    String.prototype.float = this.float
+    String.prototype.toManen = this.toManen
     return
   }
 }
