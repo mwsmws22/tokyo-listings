498e2717b843aaf962f0acf16ea52028157dc930
Mitchell Smith <mitchellwsmith@hotmail.com>
2022-01-25 21:57:25 -0800

    initial work

--- tokyo-listings-extension/background.js
+++ tokyo-listings-extension/background.js
@@ -21,19 +21,24 @@ function updateState() {
   chrome.tabs.reload()
 }
 
+function enterUrl(listingTab, appTab) {
+  chrome.tabs.sendMessage(appTab.id, { listingTab, appTab }, null, () =>
+    chrome.tabs.remove(listingTab.id)
+  )
+}
+
 function sendListingToApp() {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    const listingTab = tabs[0].url
     chrome.tabs.query({ url: 'http://localhost:8081/add' }, tabz => {
       if (tabz.length > 0) {
         chrome.tabs.update(tabz[0].id, { active: true })
-        chrome.tabs.sendMessage(tabz[0].id, { loadUrl: listingTab })
+        enterUrl(tabs[0], tabz[0])
       } else {
         chrome.tabs.create({ url: 'http://localhost:8081/add' }, tab => {
           chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
             if (info.status === 'complete' && tabId === tab.id) {
               chrome.tabs.onUpdated.removeListener(listener)
-              chrome.tabs.sendMessage(tab.id, { loadUrl: listingTab })
+              enterUrl(tabs[0], tab)
             }
           })
         })
--- tokyo-listings-extension/src/utils/PageUtils.js
+++ tokyo-listings-extension/src/utils/PageUtils.js
@@ -6,6 +6,22 @@ class PageUtils {
   }
 }
 
-chrome.runtime.onMessage.addListener(request => {
-  PageUtils.insertUrl(request.loadUrl)
+let observer
+const config = { attributes: true }
+const cbWrapper = cb => mutations =>
+  mutations.forEach(mutation => {
+    if (mutation.target?.outerText === 'Listing already in DB!') {
+      cb()
+    }
+  })
+
+chrome.runtime.onMessage.addListener((req, temp, cb) => {
+  console.log(req)
+  console.log(cb)
+  const urlLabel = document.getElementById('urlLabel')
+  // cbWrapper.bind({ cb })
+  // observer = new MutationObserver(cbWrapper(cb))
+  // observer.observe(urlLabel, config)
+
+  PageUtils.insertUrl(req.listingTab.url)
 })
